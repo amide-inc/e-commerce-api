10ba265f8e0c2e9ab195c61fdd6474d2769e2c24
Amin Uddin <aminuddin01697@gmail.com>
2021-02-24 20:34:32 +0530

    product route has been completed

--- models/item.js
+++ models/item.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const productSchema = new Schema({
+const itemSchema = new Schema({
     title : {type : String, required : true, unique : true},
     thumbnail: {type: String, required: true},
     description :{type : String, required : true},
-    keywords: {type: String}, //coma seprated String like , fashion, cloths,cotton 
+    price : {type: Number, required: true},
+    product: {type: mongoose.Types.ObjectId},
     userId : {type: mongoose.Types.ObjectId , required : true}
 });
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model('Item', itemSchema);
--- router/product-route.js
+++ router/product-route.js
@@ -1,9 +1,68 @@
 const { get } = require('./auth-route');
 
 const router = require('express').Router();
+const Product = require('../models/product');
+const checkAuth = require('../middleware/check-auth');
+const { json } = require('body-parser');
+
+router.post('/', checkAuth, (req, res) => {
+    const product =  new Product({
+        title : req.body.title,
+        description: req.body.description,
+        thumbnail: req.body.thumbnail,
+        keywords : req.body.keywords,
+        userId: req.userData.userId
+    })
+    product.save()
+           .then((result) => {
+               res.json({success : true, message: "product has been created"});
+           })
+           .catch((err) => {
+               if(err.code === 11000) {
+                res.json({success : false, message: "Product already exits"});
+               }else {
+                res.json({success : false, message: "moongoos error"});
+               }
+
+           });
+});
+
+router.get('/', (req, res) => {
+    Product.find()
+           .exec()
+           .then((result) => {
+               res.json({success : true, data: result})
+           })
+           .catch((err) =>{
+               res.json({success : false, message: err});
+           })
+});
+
+router.patch('/:id', checkAuth, (req, res) => {
+    const productId = req.params.id;
+    const data = req.body;
+    Product.updateOne({_id: productId}, {$set : data})
+           .exec()
+           .then((result) => {
+               res.json({success : true, message: "data has  been updated"});
+           })
+           .catch((error) => {
+               res.json({success : false, message: error })
+           })
 
 
-// product route will be written here
+})
+
+router.delete('/:id', checkAuth, (req, res) => {
+    const productId = req.params.id;
+    Product.deleteOne({_id: productId})
+           .exec()
+           .then((result) => {
+               res.json({success : true, message: "data has been deleted"})
+           }).catch((err) => {
+               res.json({success: false, message: err})
+           })
+});
 
 
 module.exports = router;
